51d74e674eb3bf9461ed9e0f8a9c072a7da545a4
CatMeowlet <jamesvincent.ocariza@gmail.com>
2021-01-29 12:06:08 +0800

    Modify: quilleditor theme: snow -> bubble

--- resources/js/components/editor/QuillEditor.vue
+++ resources/js/components/editor/QuillEditor.vue
@@ -174,14 +174,22 @@ export default {
                 modules: {
                     syntax: true,
                     toolbar: [
-                        [{ font: [] }, { size: [] }],
-                        ['bold', 'italic', 'underline', 'strike'],
-                        [{ color: [] }, { background: [] }],
-                        [{ script: 'super' }, { script: 'sub' }],
-                        [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block'],
-                        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-                        ['direction', { align: [] }],
-                        ['link', 'image', 'video', 'formula'],
+                        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                        ['blockquote', 'code-block'],
+
+                        [{ header: 1 }, { header: 2 }], // custom button values
+                        [{ list: 'ordered' }, { list: 'bullet' }],
+                        [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+                        [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+                        [{ direction: 'rtl' }], // text direction
+
+                        [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+                        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+                        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+                        [{ font: [] }],
+                        [{ align: [] }],
+
                         ['clean'],
                     ],
                 },
